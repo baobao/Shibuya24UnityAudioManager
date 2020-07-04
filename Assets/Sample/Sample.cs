15c5b3d83ebf6f1eb6e32b8258fc29a27d801d62
baobao <shunsuke.ohba@gmail.com>
2020-07-04 16:32:10 +0900

    Add Sample

--- Assets/Sample/Sample.cs
+++ Assets/Sample/Sample.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using info.shibuya24.Audio;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -8,23 +9,42 @@ public class Sample : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Shibuya24UnityAudioManager.InitinalizeIfNeed();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
-    [Button("PlaySFX")]
-    void PlaySfx()
+    [Button("PlayFX1")]
+    void PlayFx1()
     {
+        Shibuya24UnityAudioManager.Play("se/se_fx1");
     }
 
-    [Button("PlayBGM")]
-    void PlayBGM()
+    [Button("PlayFX2")]
+    void PlayFx2()
     {
+        Shibuya24UnityAudioManager.Play("se/se_fx2");
+    }
+
+    [Button("PlayFX3")]
+    void PlayFx3()
+    {
+        Shibuya24UnityAudioManager.Play("se/se_fx3");
+    }
+
+    [Button("PlayBGM2")]
+    void PlayBGM1()
+    {
+        Shibuya24UnityAudioManager.Play("bgm/bgm_test1");
+    }
+
+    [Button("PlayBGM2")]
+    void PlayBGM2()
+    {
+        Shibuya24UnityAudioManager.Play("bgm/bgm_test2");
     }
 
     [SerializeField] private AudioClip _clip1;
